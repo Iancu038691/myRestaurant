d7b4ab194d4bc41f29450d15483cbc4751868093
Iancu Stefan-George <iancu.stefan038691@gmail.com>
2023-10-08 01:23:37 +0300

    !!!

--- src/index.js
+++ src/index.js
@@ -37,7 +37,11 @@ header.appendChild(aboutus);
 header.appendChild(location);
 
 
+const startText = document.querySelector('div');
+startText.classList.add('startText');
+startText.textContent = 'Welcome to our restaurant, how can we serve you';
 
+middle.appendChild(startText);
 
 
 
